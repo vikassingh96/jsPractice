2e348e7e8e602dd6161435966dccae3a0f2448e6
vikassingh96 <rajputtwo@gmail.com>
2020-04-21 15:17:15 +0530

    type coercion

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+/********variables and data types
+
 // strings
-var firstName = 'john';
+var firstName = 'vikas';
 console.log(firstName);  
 
 // numbers
@@ -9,3 +11,21 @@ console.log(digit);
 // boolean logic
 var fullAge = true;
 console.log(fullAge); 
+
+*/
+  
+/* Variables mutation and type coercion
+ */
+
+var firstName = 'vikas';
+var Age = '23';
+
+console.log(firstName + ' ' + Age);
+
+var job, isMarried;
+job = 'Engineer';
+isMarried = false;
+
+console.log(firstName + ' is a ' + Age + ' year old ' + job + ' is he married ? ' + isMarried);
+ 
+
