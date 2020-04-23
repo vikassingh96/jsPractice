8545717c78bdae6047d079fa5870d9091a0e3f08
vikassingh96 <rajputtwo@gmail.com>
2020-04-23 12:25:08 +0530

    coding challenge

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ console.log(average);
 
 /************************************
  * Boolean logic
- */
+ 
 
 var firstName = 'vikas';
 var age = 20;
@@ -121,6 +121,38 @@ if (age < 13) {
     console.log(firstName + ' ' + 'is a adult');
 } else {
     console.log (firstName + ' ' + 'is a men');
-}
- 
+} 
+ */
 
+ /*********************************
+  * CODING CHALLENGE 2
+*/
+
+/*
+John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+
+1. Calculate the average score for each team
+
+2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
+
+3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
+
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
+
+5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
+
+  //solution*/
+
+var teamJohn = (89 + 120 + 103)/ 3;
+var teamMike = (116 + 94 + 123)/ 3;
+var teamMary = (97 + 134 + 105)/ 3;
+
+if (teamJohn > teamMike && teamJohn > teamMary) {
+    console.log(teamJohn + ' John is the Winner');
+} else if (teamMike > teamJohn &&  teamMike > teamMary) {
+    console.log(teamMike + ' Mike is the Winner');
+} else if (teamMary > teamJohn && teamMary > teamMike) {
+    console.log(teamMary + ' Mary is the Winner');
+} else { 
+    console.log(' match is draw ');
+}
